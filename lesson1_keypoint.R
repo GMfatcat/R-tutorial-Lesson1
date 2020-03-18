9ecd1055b049cb2a12393a29b800a2ebe1d79220
GMfatcat <58526756+GMfatcat@users.noreply.github.com>
2020-03-19 05:55:56 +0800

    Add files via upload

---
+++ lesson1_keypoint.R
@@ -0,0 +1,18 @@
+#please run the code section by section
+#install ggplot2 package
+install.packages(ggplot2)
+
+#-----------------------
+#press ctrl+l can help you wipe out everything on the table
+#-----------------------
+#Factor vector
+x <- c("cat","bird","dog")
+y <- c(x,"cat","puppy","dog")
+x
+#-----------------------
+x1 <- as.factor(x)
+x1
+#-----------------------
+as.numeric(x1)
+
+
